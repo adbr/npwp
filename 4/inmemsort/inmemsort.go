003758e6468ee31b9278ea374f8340cd72344bb8
adbr <adbr@kwarc.home.lan>
2014-10-17 21:12:47 +0200

    Dodanie 4/inmemsort

---
+++ 4/inmemsort/inmemsort.go
@@ -0,0 +1,18 @@
+// 2014-10-17 Adam Bryt
+
+// Narzędzia Programistyczne w Pascalu,
+// rozdział 4.3 "Sortowanie tekstu",
+// program inmemsort.
+//
+// NAZWA
+//
+// SPOSÓB UŻYCIA
+//
+// OPIS
+//
+// PRZYKŁADY
+//
+package main
+
+func main() {
+}
