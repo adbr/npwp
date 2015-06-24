a800e0487bbd48dbcc635c7d3dd133afbc53aa23
Adam Bryt <adam.bryt@gmx.com>
2015-06-24 18:46:05 +0200

    6/edit: Początek programu edit

---
+++ 6/edit/doc.go
@@ -0,0 +1,21 @@
+// 2015-06-24 Adam Bryt
+
+/*
+Narzędzia Programistyczne w Pascalu,
+rozdział 6 "Redagowanie tekstów",
+program edit.
+
+NAZWA
+
+edit - edytor plików tekstowych
+
+SPOSÓB UŻYCIA
+
+OPIS
+
+UWAGI
+
+PRZYKŁADY
+
+*/
+package main
